33681e2ad72348470367057bb2bc7ad9abdca8db
github1gmx <github1@gmx.com>
2017-03-12 23:22:13 +0100

    Modified logging

--- src/main/java/testing/Testing.java
+++ src/main/java/testing/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		System.out.println("Testing!!!");
-        System.out.println("Hello World");
+        System.out.println("Hello World!!");
 	}
 
 }
